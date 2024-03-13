9783734d578a5d8829937e9fdbd4c4fa94aa30ee
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-03-13 15:25:40 +0500

    test Local Storege

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
+
+  const LS = localStorage.getItem('local')
+ 
+  function handlerClick(){
+    setCount(count => count + 1)
+    
+    localStorage.setItem('local', count.toString())
+
+  }
+
   return (
     
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handlerClick}>
           count is {count}
         </button>
+        <p>{count.toString()}</p>
+        <h1>{LS}</h1>
     
       </div>
  
