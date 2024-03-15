af6d5b477953a04330e0dcfc9dad602ffea2c6c2
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-03-15 15:41:27 +0500

    fix load app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import { useState } from "react";
 import "./App.css";
-import { LoadingApp } from "./components/LoadingApp";
 import Home from "./components/Home";
 
 function App() {
-	const [isLogo, setIsLogo] = useState(true);
-
-	setTimeout(() => {
-		setIsLogo(false);
-	}, 1500);
-
-	if (isLogo) {
-		return <LoadingApp />;
-	} else {
-		return <Home />;
-	}
+	return <Home />;
 }
 
 export default App;
